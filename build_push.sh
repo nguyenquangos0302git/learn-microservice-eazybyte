44c48d36866720d720dad44c280f10ff15b9cc8a
nguyen quang <nguyenquangos0302git@gmail.com>
2024-10-30 07:23:47 +0700

    upload

--- build_push.sh
+++ build_push.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-cd section12
+cd section13
 
 cd accounts
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/accounts:s12
+docker push docker.io/nguyenquangos0302app/accounts:s13
 cd ..
 
 cd cards
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/cards:s12
+docker push docker.io/nguyenquangos0302app/cards:s13
 cd ..
 
 cd configserver
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/configserver:s12
+docker push docker.io/nguyenquangos0302app/configserver:s13
 cd ..
 
 cd eurekaserver
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/eurekaserver:s12
+docker push docker.io/nguyenquangos0302app/eurekaserver:s13
 cd ..
 
 cd gatewayserver
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/gatewayserver:s12
+docker push docker.io/nguyenquangos0302app/gatewayserver:s13
 cd ..
 
 cd loans
 mvn clean compile jib:dockerBuild
-docker push docker.io/nguyenquangos0302app/loans:s12
+docker push docker.io/nguyenquangos0302app/loans:s13
+cd ..
+
+cd message
+mvn clean compile jib:dockerBuild
+docker push docker.io/nguyenquangos0302app/message:s13
 cd ..
